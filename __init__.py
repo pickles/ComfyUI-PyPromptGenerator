6ca7e1da6061b12da9aadc3efea6fa3b6dbf79a4
SHIBATA, Manabu <manabu.shibata@gmail.com>
2025-08-30 03:16:08 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""Top-level package for pyprompt_generator."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__version__ = "0.0.1"
+
+from .src.pyprompt_generator.nodes import NODE_CLASS_MAPPINGS
+from .src.pyprompt_generator.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for pyprompt_generator."""
